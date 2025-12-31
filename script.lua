bc74e91208e91ec68ef47e239bbf9a1f8fe82a4c
kenancigdem010-sudo <kenancigdem010@gmail.com>
2025-12-31 06:32:21 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,121 @@
+--[[ EctacsyzObf Protected Script ]]
+local _ubznepjej=(5038^2)-3805
+local _zschxbokd=(4401^2)-4223
+local _xcmzflynu=(4274^2)-4255
+local _dwurixwaa=(3207^2)-4558
+local _uknpbofij=(9480^2)-2314
+local _bgqyxqqkr=(8411^2)-3765
+local _bwnpjabam=(4858^2)-2204
+local _znjcsdjnx=(3253^2)-2454
+local _gwhqocjzj=(4367^2)-3063
+local _gdozhiumx=(4421^2)-4949
+local _jqtasttwm=(2439^2)-4129
+local _pvshwwpar=(2846^2)-4234
+local _rnwywkzns=(8397^2)-3450
+local _lqrdzseij=(2312^2)-4109
+local _xjngdbrho=(9059^2)-2846
+local _sowxygirf=(6283^2)-2782
+local _kxgojmvoz=(6031^2)-3398
+local _vrbgoryjx=(3544^2)-4603
+local _clubsfhpw=(9701^2)-1985
+local _xsvpcvooe=(752^2)-4287
+local _qsmhrksdm=(2243^2)-2137
+local _iejalvuss=(5543^2)-3400
+local _xqytjdrxx=(2670^2)-3994
+local _rraqyjwxu=(3514^2)-1241
+local _pcqdkptah=(8302^2)-630
+local _hmlyhszyb=(3479^2)-1313
+
+local _ookinzzvc="64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,64,54,64,67,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,65,54,64,59,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,63,54,63,65,54,64,65,54,64,60,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,64,54,64,65,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,65,54,64,61,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,67,54,63,65,54,64,64,54,64,65,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,66,54,63,65,54,64,65,54,64,59,54,63,65,54,64,63,54,65,58,54,63,65,54,64,65,54,64,60,54,63,65,54,64,64,54,64,67,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,66,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,64,54,64,63,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,64,54,64,63,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,63,54,63,65,54,64,64,54,64,64,54,63,65,54,64,64,54,64,66,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,66,54,63,65,54,64,65,54,64,59,54,63,65,54,64,63,54,65,58,54,63,65,54,64,64,54,64,67,54,63,65,54,64,64,54,64,66"
+
+local function _uvpaajzja(_xdmvvtupc,_mnldoveum)
+  local _vboauxbaz={}
+  for _jvcgsdavs in string.gmatch(_xdmvvtupc,"[^,]+") do
+    _vboauxbaz[#_vboauxbaz+1]=string.char(tonumber(_jvcgsdavs)-_mnldoveum)
+  end
+  return table.concat(_vboauxbaz)
+end
+
+
+local _byavohiib=(2794^2)-664
+local _bzeghtsyy=(325^2)-2500
+local _qxbwwsgmg=(4432^2)-1006
+local _svbqagoda=(1617^2)-672
+local _owtrxqoyt=(9272^2)-2346
+local _ujsrjcwga=(150^2)-2234
+local _cgrtkekfo=(9446^2)-3317
+local _lyrrvdhzj=(9059^2)-3230
+local _trfuzioje=(8651^2)-3297
+local _swbwyjluc=(6653^2)-166
+
+local _iflhfhnws=_uvpaajzja(_ookinzzvc,10)
+
+local _jqoropjrr=(3844^2)-547
+local _spquytfyj=(6863^2)-1451
+local _oizazimsm=(1344^2)-1958
+local _vorvsamdn=(6014^2)-4700
+local _tzgdupdbo=(3709^2)-4679
+local _gqbqvxnfq=(6795^2)-2366
+local _zqulltuhg=(927^2)-4431
+local _avsuufuwz=(2139^2)-3487
+
+local _iflhfhnws=_uvpaajzja(_iflhfhnws,13)
+
+local _oatbmtjiz=(7853^2)-3876
+local _aiviisskz=(6659^2)-2778
+local _mzvmtgffg=(4394^2)-2348
+local _abnpdnjsz=(8729^2)-4920
+local _dsnyldnom=(2494^2)-2956
+local _ozcoadxaw=(4932^2)-4640
+local _yiscqycmu=(8278^2)-1014
+local _mcyqgptdu=(3661^2)-517
+local _jgrqviyzu=(1758^2)-3632
+
+local _iflhfhnws=_uvpaajzja(_iflhfhnws,5)
+
+local _mukzebqeh=(1726^2)-1769
+local _tdpxzcpbs=(5541^2)-1167
+local _iqykomvjk=(7325^2)-909
+local _lnqfejonj=(4739^2)-2920
+local _ughuohqno=(4422^2)-774
+local _jllqfswvl=(2637^2)-2012
+local _kivwmyklb=(1436^2)-3871
+local _nghklxzht=(149^2)-2802
+local _wzurazkyi=(7356^2)-3987
+
+local _iflhfhnws=_uvpaajzja(_iflhfhnws,13)
+
+local _prsizbvws=(1758^2)-310
+local _agggnttiy=(905^2)-2932
+local _ywagwzwtn=(3614^2)-2838
+local _mcadqdbhj=(9650^2)-2265
+local _uwosrckme=(968^2)-4685
+local _dnntdvsup=(2453^2)-2767
+local _rggvyzzjs=(1923^2)-2803
+local _ltkqpoacq=(2163^2)-1083
+local _iehxvqmka=(3560^2)-4355
+local _kmqlzlyfl=(6798^2)-813
+local _zydmsswyw=(3711^2)-1171
+local _dkgtubgwg=(7074^2)-2530
+local _fltpkqjqc=(5502^2)-3685
+local _nqhodqidi=(9530^2)-3814
+local _bqsrjgewf=(8821^2)-1392
+local _rsynqajuw=(4847^2)-3502
+local _kikuygqdp=(2776^2)-2226
+local _vvtbykgul=(6986^2)-249
+local _njvlxtbuf=(8240^2)-132
+local _mkfvqcoip=(2697^2)-949
+local _xjlakqhfy=(6501^2)-4269
+local _rnctiwibx=(7795^2)-4090
+local _gjflhtknm=(6531^2)-4123
+local _svmxdfyts=(2456^2)-3641
+local _fqcdviwud=(8119^2)-3368
+local _jhtnjvubn=(7510^2)-3479
+local _kwikdfyow=(652^2)-1381
+local _gpzsntlia=(2403^2)-3217
+local _wjayyffek=(6150^2)-2964
+local _hnzofybil=(513^2)-1335
+local _hruaikoan=(4123^2)-2914
+
+local _fmlyinqfg=load(_iflhfhnws)
+pcall(_zflqgujxc)
